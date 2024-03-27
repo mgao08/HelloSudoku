ee17c928ae8910fe34007b097f4d94170bf70954
Monica Gao <mgaozh@gmail.com>
2024-03-26 17:55:06 -0700

    Finished login section for small & large screen sizes.

--- views/js/script.js
+++ views/js/script.js
@@ -6,7 +6,15 @@
 
 
 window.onload = () => {
+   // TODO: remove the display none before committing
+   let sections = document.querySelectorAll('section');
+   sections.forEach(section => {
+      section.setAttribute('style', 'display: none !important');
+         /** Used setAttribute cuz need to overwrite important */
+   });
 
+   let login  = document.getElementById("login");
+   login.setAttribute('style', 'display: flex !important');
 };
 
 })();
