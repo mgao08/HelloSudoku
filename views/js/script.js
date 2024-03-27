f13fc8081d5c7013a70e17e5e2cd68164caead0d
Monica Gao <mgaozh@gmail.com>
2024-03-27 16:45:14 -0700

    Added basic switch section JS function. Fixed minor display issue where nav menu does not cover the whole screen under small screen sizes.

--- views/js/script.js
+++ views/js/script.js
@@ -1,36 +1,88 @@
 // IIFE
 (() => {
 
-// general use modal toggler
-
-// collection of setup statements that needs to be run onload
-const setup = () => {
-   // prevent form button from submitting onclick
-   let modalTrigger = document.querySelector('#registerBtn');
-   modalTrigger.onclick = (evt) => {
-      evt.preventDefault();
-   };
-   let registerBtn = document.querySelector('#continueRegister');
-   registerBtn.onclick = () => {
-      console.log("Continue with register process");
-   }
-}
-
-
-window.onload = () => {
-   // TODO: remove the display none before committing
+// switch section function
+const toSection = (text) => {
    let sections = document.querySelectorAll('section');
    sections.forEach(section => {
       section.setAttribute('style', 'display: none !important');
          /** Used setAttribute cuz need to overwrite important */
    });
 
-   let login = document.getElementById("login");
-   // login.setAttribute('style', 'display: flex !important');
-   let register = document.getElementById("register");
-   register.setAttribute('style', 'display: flex !important');
+   let dest = text.toLowerCase();
+   if (dest.includes("dashboard")) {
+      dest = "dashBoard";  /** corresponding id */
+   } else if (dest.includes("daily")) {
+      dest = "gamePane";
+   } else if (dest.includes("select")) {
+      dest = "selectLevel";
+   } else if (dest.includes("register")) {
+      dest = "register";
+   } else if (dest.includes("admin")) {
+      dest = "admin";
+   } else {
+      dest = "login";
+   }
+
+   // hide nav menu if under vw 1200px
+   if (window.innerWidth < 1200) {
+      document.getElementById('navList').classList.remove("show");
+   }
+   document.getElementById(dest).setAttribute('style', 'display: flex !important');
+};
+
+
+// login function
+const loginWith = (dummy) => {
+   // Append login functions here
+
+
+   toSection('dashboard');
+}
+
+
+// register function
+const registerWith = (dummy) => {
+   // Append login functions here
+
+
+   loginWith('auto login with new username & password');
+}
+
+
+// collection of setup statements that needs to be run onload
+const setup = () => {
+   // setup event listeners for links for section switch
+   let sectionLinks = document.querySelectorAll('.sectionLink');
+   sectionLinks.forEach(link => {
+      link.onclick = () => {
+         toSection(link.innerText);
+      }
+   });
+
+   // prevent form button from submitting onclick
+   let modalTrigger = document.querySelector('#registerBtn');
+   modalTrigger.onclick = (evt) => {
+      evt.preventDefault();
+   };
+
+   // login & register event handlers
+   let registerBtn = document.querySelector('#continueRegister');
+   registerBtn.onclick = () => {
+      registerWith('dummy data');
+   }
+
+   let loginBtn = document.querySelector('#loginBtn');
+   loginBtn.onclick = () => {
+      loginWith('dummy data');
+   };
+};
+
+
+window.onload = () => {
 
    setup();
+   toSection('login');
 };
 
 })();
